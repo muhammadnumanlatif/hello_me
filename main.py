e07ada8cdd75eecc10dad2c317b2c0dfffbfd30e
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2021-08-19 21:24:02 +0500

    hello with kivy

---
+++ main.py
@@ -0,0 +1,54 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class SayHello(App):
+    def build(self):
+        #layout
+        self.window = GridLayout()
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        #column
+        self.window.cols = 1
+        #image widget
+        self.window.add_widget(Image(source="logo.png"))
+        #Label
+        self.greeting = Label(
+            text="My name is ",
+            font_size= 18,
+            color='#00FFCE'
+        )
+        self.window.add_widget(self.greeting)
+
+        #text input
+        self.user = TextInput(
+            multiline=False,
+            padding_y = (20,20),
+            size_hint = (1, 0.5)
+        )
+        self.window.add_widget(self.user)
+
+        #button
+        self.button = Button(
+            text="Greeting",
+            size_hint = (1,0.5),
+            bold = True,
+            background_color = "#00FFCE",
+            background_normal = ""
+        )
+        self.button.bind(on_press=self.callback)
+        self.window.add_widget(self.button)
+
+        return self.window
+
+    def callback(self, instance):
+        self.greeting.text = "Hello " + self.user.text + "!"
+
+
+        #end
+
+if __name__ == "__main__":
+    SayHello().run()
